a118d1bfa22c48f97f08510d1154df6a0e56e655
Josef Kuchař <josef.kuchar267@gmail.com>
2021-12-02 10:28:28 +0100

    Day 2

---
+++ src/day02/index.ts
@@ -0,0 +1,80 @@
+import { test, readInput } from "../utils/index";
+
+interface Command {
+  dir: string;
+  val: number;
+}
+
+const prepareInput = (rawInput: string) =>
+  rawInput
+    .trim()
+    .split("\n")
+    .map((line) => {
+      const parts = line.split(" ");
+      return {
+        dir: parts[0],
+        val: parseInt(parts[1]),
+      };
+    });
+
+const input = prepareInput(readInput("day02"));
+
+const goA = (input: Command[]) => {
+  let [x, y] = [0, 0, 0];
+  input.forEach((command) => {
+    switch (command.dir) {
+      case "forward":
+        x += command.val;
+        break;
+      case "down":
+        y += command.val;
+        break;
+      case "up":
+        y -= command.val;
+        break;
+    }
+  });
+  return x * y;
+};
+
+const goB = (input: Command[]) => {
+  let [x, y, aim] = [0, 0, 0];
+  input.forEach((command) => {
+    switch (command.dir) {
+      case "forward":
+        x += command.val;
+        y += command.val * aim;
+        break;
+      case "down":
+        aim += command.val;
+        break;
+      case "up":
+        aim -= command.val;
+        break;
+    }
+  });
+  return x * y;
+};
+
+/* Tests */
+
+const testInput = prepareInput(`
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`);
+
+test(goA(testInput), 150);
+test(goB(testInput), 900);
+
+/* Results */
+
+console.time("Time");
+const resultA = goA(input);
+const resultB = goB(input);
+console.timeEnd("Time");
+
+console.log("Solution to part 1:", resultA);
+console.log("Solution to part 2:", resultB);
